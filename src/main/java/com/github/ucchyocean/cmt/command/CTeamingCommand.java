440d574ae0bd1c6d2d5229d73f1d67e975ba9390
ucchyocean <ocean163@hotmail.com>
2013-02-10 20:41:28 +0900

    ColorMeTeaming v1.4.0 : Added "/ct add (group) (user)" command. ColorMeTeaming v1.4.0 : Added "/ct remove (group)" command.

--- src/main/java/com/github/ucchyocean/cmt/command/CTeamingCommand.java
+++ src/main/java/com/github/ucchyocean/cmt/command/CTeamingCommand.java
@@ -3,17 +3,27 @@
  */
 package com.github.ucchyocean.cmt.command;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
+import com.github.ucchyocean.cmt.ColorMeTeaming;
 import com.github.ucchyocean.cmt.ColorMeTeamingConfig;
+import com.github.ucchyocean.cmt.Utility;
 
 /**
  * @author ucchy
  * colorteaming(ct)コマンドの実行クラス
  */
 public class CTeamingCommand implements CommandExecutor {
+
+    private static final String PREERR = ChatColor.RED.toString();
+    private static final String PREINFO = ChatColor.GRAY.toString();
 
     /**
      * @see org.bukkit.plugin.java.JavaPlugin#onCommand(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
@@ -30,6 +40,41 @@ public class CTeamingCommand implements CommandExecutor {
             ColorMeTeamingConfig.reloadConfig();
             sender.sendMessage("config.ymlの再読み込みを行いました。");
             return true;
+
+        } else if ( args.length >= 2 && args[0].equalsIgnoreCase("remove") ) {
+
+            Hashtable<String, ArrayList<Player>> members = ColorMeTeaming.getAllColorMembers();
+            String group = args[1];
+            if ( !members.containsKey(group) ) {
+                sender.sendMessage(PREERR + "グループ " + group + " は存在しません。");
+                return true;
+            }
+            for ( Player p : members.get(group) ) {
+                ColorMeTeaming.removePlayerColor(p);
+                p.sendMessage(PREINFO + "グループ " + group + " が解散しました。");
+            }
+            sender.sendMessage(PREINFO + "グループ " + group + " が解散しました。");
+
+            // TODO: 保護領域の更新
+
+        } else if ( args.length >= 3 && args[0].equalsIgnoreCase("add") ) {
+
+            String group = args[1];
+            if ( !Utility.isValidColor(group) ) {
+                sender.sendMessage(PREERR + "グループ " + group + " はColorMeに設定できないグループ名です。");
+                return true;
+            }
+
+            Player player = ColorMeTeaming.getPlayerExact(args[2]);
+            if ( player == null ) {
+                sender.sendMessage(PREERR + "プレイヤー " + args[2] + " は存在しません。");
+                return true;
+            }
+
+            ColorMeTeaming.setPlayerColor(player, group);
+
+            // TODO: 保護領域の更新
+
         }
 
         return false;
