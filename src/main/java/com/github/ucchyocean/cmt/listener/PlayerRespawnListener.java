e80da51020a59e7453c1dc548b2eca156c2ad97f
ucchyocean <ocean163@hotmail.com>
2013-03-03 18:17:53 +0900

    ColorMeTeaming v1.4.3 : Changed PlayerQuitEvent priority to Highest.

--- src/main/java/com/github/ucchyocean/cmt/listener/PlayerRespawnListener.java
+++ src/main/java/com/github/ucchyocean/cmt/listener/PlayerRespawnListener.java
@@ -6,6 +6,7 @@ package com.github.ucchyocean.cmt.listener;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
@@ -22,7 +23,7 @@ public class PlayerRespawnListener implements Listener {
      * Playerがログアウトしたときに発生するイベント
      * @param event
      */
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerRespawn(PlayerRespawnEvent event) {
 
         Player player = event.getPlayer();
