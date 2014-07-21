5a6b0808a9a02252d31b153a5c4584eb9a971679
ucchy <ocean163@hotmail.com>
2014-07-21 21:29:33 +0900

    ColorTeaming v2.3.9 : Fixed issue #109.

--- src/main/java/com/github/ucchyocean/ct/command/CRemoveCommand.java
+++ src/main/java/com/github/ucchyocean/ct/command/CRemoveCommand.java
@@ -41,10 +41,12 @@ public class CRemoveCommand implements TabExecutor {
         if ( args[0].equalsIgnoreCase("on") ) {
             setColorRemoveOnDeath(sender, true);
             setColorRemoveOnQuit(sender, true);
+            setColorRemoveOnChangeWorld(sender, true);
             return true;
         } else if ( args[0].equalsIgnoreCase("off") ) {
             setColorRemoveOnDeath(sender, false);
             setColorRemoveOnQuit(sender, false);
+            setColorRemoveOnChangeWorld(sender, false);
             return true;
         } else if ( args[0].equalsIgnoreCase("death") && args.length >= 2 ) {
             if ( args[1].equalsIgnoreCase("off") ) {
@@ -58,6 +60,13 @@ public class CRemoveCommand implements TabExecutor {
                 setColorRemoveOnQuit(sender, false);
             } else {
                 setColorRemoveOnQuit(sender, true);
+            }
+            return true;
+        } else if ( args[0].equalsIgnoreCase("changeworld") && args.length >= 2 ) {
+            if ( args[1].equalsIgnoreCase("off") ) {
+                setColorRemoveOnChangeWorld(sender, false);
+            } else {
+                setColorRemoveOnChangeWorld(sender, true);
             }
             return true;
         }
@@ -94,6 +103,20 @@ public class CRemoveCommand implements TabExecutor {
     }
 
     /**
+     * ワールド変更時のチーム離脱の有効/無効を切り替える
+     * @param sender メッセージ送信先
+     * @param enable 有効/無効
+     */
+    private void setColorRemoveOnChangeWorld(CommandSender sender, boolean enable) {
+        ColorTeamingConfig config = plugin.getCTConfig();
+        config.setColorRemoveOnChangeWorld(enable);
+        config.saveConfig();
+        String msg = enable ? "有効" : "無効";
+        sender.sendMessage(ChatColor.GRAY +
+                "ワールド変更時のチーム離脱が" + msg + "になりました。");
+    }
+
+    /**
      * @see org.bukkit.command.TabCompleter#onTabComplete(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
      */
     @Override
@@ -104,7 +127,7 @@ public class CRemoveCommand implements TabExecutor {
 
             String prefix = args[0].toLowerCase();
             ArrayList<String> commands = new ArrayList<String>();
-            for ( String c : new String[]{"on", "off", "death", "quit"} ) {
+            for ( String c : new String[]{"on", "off", "death", "quit", "changeworld"} ) {
                 if ( c.startsWith(prefix) ) {
                     commands.add(c);
                 }
@@ -112,7 +135,9 @@ public class CRemoveCommand implements TabExecutor {
             return commands;
 
         } else if ( args.length == 2 &&
-                (args[0].equalsIgnoreCase("death") || args[0].equalsIgnoreCase("quit")) ) {
+                        (args[0].equalsIgnoreCase("death") ||
+                        args[0].equalsIgnoreCase("quit") ||
+                        args[0].equalsIgnoreCase("changeworld") ) ) {
 
             String prefix = args[1].toLowerCase();
             ArrayList<String> commands = new ArrayList<String>();
